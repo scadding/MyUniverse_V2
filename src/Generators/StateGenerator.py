92a05f5c4f4ea7cc5d1fe071cce04e234d2e6540
Doug Scadding <doug.scadding@gmail.com>
2025-03-23 17:37:38 +0900

    changed to configuration

--- src/Generators/StateGenerator.py
+++ src/Generators/StateGenerator.py
@@ -1,10 +1,12 @@
 
 from src.Generators.tablegen import table
+from src.Configuration import Configuration
 
 class Generator:
     def __init__(self):
+        config = Configuration()
         self.tm = table.tableMgr()
-        table.walktree('Data/Tables', self.tm.addfile)
+        table.walktree(config.getValue("Data", "directory"), self.tm.addfile)
         self.parameters = dict()
         self.parameters['Seed'] = ['', '0']
         self.parameters['Group'] = self.GetGeneratorGroups()
